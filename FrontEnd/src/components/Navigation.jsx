a31bd223ba38f4499c32cfabae3901a76a4a1349
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-11-12 14:04:48 +0500

    complete frontend

---
+++ FrontEnd/src/components/Navigation.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { mobMenu } from '../utility/imports'
+
+const Navigation = () => {
+
+    const [Listcolor, setListColor] = useState(true)
+    const [AddNewcolor, setAddNewColor] = useState(false)
+    const [AddContactColor, setAddContact] = useState(false)
+    const [MobMenu, setmobMenu] = useState(false)
+
+    const handleList = () => {
+        setListColor(true)
+        setAddNewColor(false)
+        setAddContact(false)
+        setmobMenu(false)
+    }
+
+    const handleAddNew = () => {
+        setAddNewColor(true)
+        setListColor(false)
+        setAddContact(false)
+        setmobMenu(false)
+    }
+
+    const handleContact = () => {
+        setAddContact(true)
+        setAddNewColor(false)
+        setListColor(false)
+        setmobMenu(false)
+    }
+
+    const handleMobMenu = () => {
+       setmobMenu(!MobMenu)
+    }
+
+    return (
+        <>
+            <img className='mobMenu' onClick={handleMobMenu} src={mobMenu} alt="mobMenu" />
+            <header>
+                <h1>Awesome Books</h1>
+                <ul className='NavbuttonsDiv'>
+                    <Link to="/" onClick={handleList}><li className={Listcolor ? 'listSelected' : 'Navbtn'} >List</li></Link>
+                    <Link to="/AddBook" onClick={handleAddNew}><li className={AddNewcolor ? 'addNewSelected' : 'Navbtn'}>Add New</li></Link>
+                    <Link to="/Contact" onClick={handleContact}><li className={AddContactColor ? 'contactSelected' : 'Navbtn'}>Contact</li></Link>
+                </ul>
+            </header>
+            {MobMenu? 
+            <div className='mobDivForBlur'>
+                <ul className='MobNavbuttons'>
+                    <Link to="/" onClick={handleList}><li className={Listcolor ? 'listSelected' : 'Navbtn'} >List</li></Link>
+                    <Link to="/AddBook" onClick={handleAddNew}><li className={AddNewcolor ? 'addNewSelected' : 'Navbtn'}>Add New</li></Link>
+                    <Link to="/Contact" onClick={handleContact}><li className={AddContactColor ? 'contactSelected' : 'Navbtn'}>Contact</li></Link>
+                </ul>
+            </div>
+            : ""}
+        </>
+    )
+}
+
+export default Navigation
